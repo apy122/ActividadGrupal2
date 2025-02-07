52ca1114a85fc5d8144c49e08255b662c645a4bc
AlessandroGarcia <alessandro.garcia234@comunidadunir.net>
2025-02-07 18:48:19 +0100

    Primera versión de la clase LogaritmosyExponenciales.

--- Calculadora/src/main/java/javabean/LogaritmosyExponenciales.java
+++ Calculadora/src/main/java/javabean/LogaritmosyExponenciales.java
@@ -1,4 +1,45 @@
 package javabean;
 
+/**
+ * Esta clase proporciona funciones para calcular logaritmos y exponenciales
+ * @author Aless-Mereu
+ */
 public class LogaritmosyExponenciales {
-}
+
+        /**
+         * Calcula el logaritmo natural/neperiano (base e) de un número.
+         * @param x Número de entrada (x > 0)
+         * @return log_e(x)
+         * @throws IllegalArgumentException si x <= 0
+         */
+        public static double logaritmoNatural(double x) {
+            if(x <= 0)
+                throw new IllegalArgumentException("El numero no puede ser menor mo igual a cero");
+            return Math.log(x);
+        }
+
+        /**
+         * Calcula el logaritmo en base 10 de un número.
+         * @param x Número de entrada (x > 0)
+         * @return log_10(x)
+         * @throws IllegalArgumentException si x <= 0
+         */
+        public static double logaritmoBase10(double x) {
+            if(x <= 0)
+                throw new IllegalArgumentException("El numero no puede ser menor mo igual a cero");
+            return Math.log10(x);
+        }
+
+        /**
+         * Calcula la exponencial de (e^x).
+         * @param x Exponente
+         * @return e^x
+         */
+        public static double exponencial(double x) {
+            return Math.exp(x);
+        }
+
+
+    }
+
+
