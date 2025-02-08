a590431e8e186969eb9e9d98e76a126e5fae4877
AlessandroGarcia <alessandro.garcia234@comunidadunir.net>
2025-02-08 13:19:56 +0100

    Actualización de la clase, documentación realizada, menú de clase completado e inicio de la construcción del menú principal del proyecto

---
+++ Calculadora/src/main/java/org/example/MainMenu.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class MainMenu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char opcion;
+
+        do {
+            System.out.println("Que quieres hacer?");
+            System.out.println("1. SUMAR");
+            System.out.println("2. RESTAR");
+            System.out.println("3. MULTIPLICAR");
+            System.out.println("4. DIVIDIR");
+            System.out.println("5. CALCULAR MODULO");
+            System.out.println("6. CONVERSION DE UNIDADES");
+            System.out.println("7. ESTADISTICA");
+            System.out.println("8. TRIGONOMETRIA");
+            System.out.println("9. CALCULO LOGARITMICO Y EXPONENCIAL");
+            System.out.println("0. Salir");
+
+            opcion = sc.next().charAt(0);
+            switch (opcion) {
+                case '1':
+                    // En cuanto están hechas las clases y métodos se completará
+            }
+        }while(sc.nextInt() !=0);
+        }
+    }
+}
