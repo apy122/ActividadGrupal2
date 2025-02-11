c062c0b5137e4f7a37ee7e2768c91c06144bb686
david-sanara <david.sanchez769@comunidadunir.net>
2025-02-11 10:45:02 +0100

    Método para el módulo realizado con excepcion de divisor cero

--- Calculadora/src/main/java/javabean/Modulo.java
+++ Calculadora/src/main/java/javabean/Modulo.java
@@ -1,4 +1,31 @@
 package javabean;
 
+/** Clase Módulo dentro de la Calculadora grupal.
+ * <p>Ejemplo de uso</p>
+ * @author David Sánchez
+ * @version 1.0
+ */
+
 public class Modulo {
+
+    /**
+     * Esta clase tiene dos funciones:
+     *
+     * 1º Cálculo del módulo de dos números enteros. (Residuo de una división entera).
+     * @param a es el dividendo.
+     * @param b es el divisor.
+     * @return devuelve el residuo de la división entera.
+     * @throws IllegalArgumentException si el divisor es 0.
+     */
+
+    public int modulo(int a, int b){
+
+        if(b==0){
+            return ArithmeticException("El divisor no puede ser cero");
+        }
+        return a%b;
+    }
+
+
+
 }
