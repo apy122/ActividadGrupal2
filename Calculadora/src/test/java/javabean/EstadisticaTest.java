f065e44803dc0d2a174869b7dd41e8109db80c70
david-sanara <david.sanchez769@comunidadunir.net>
2025-02-14 17:37:42 +0100

    Merge branch 'David' of C:\DAM\0102_ED\Java_Workspace\AD2_Javadoc_JUnit\ActividadGrupal2 with conflicts.

--- Calculadora/src/test/java/javabean/EstadisticaTest.java
+++ Calculadora/src/test/java/javabean/EstadisticaTest.java
@@ -1,5 +1,50 @@
 package javabean;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class EstadisticaTest {
 
+    private Estadistica estadistica;
+
+    @BeforeEach
+    void setUp() {
+
+        Estadistica estadistica = new Estadistica()
+    }
+
+    @Test
+    void mediaAritmeticaPositivos() {
+        double[] numeros = {1,7,9,5}
+
+        double resultado = Estadistica.mediaAritmetica(numeros);
+
+
+
+
+    }
+    @Test
+    void mediaAritmeticaNegativos() {
+        Estadistica.mediaAritmetica(-1,-7,-9,-5);
+
+    }
+   @Test
+   void mediaAritmeticaCero(0) {
+       Estadistica.mediaAritmetica();
+
+    }
+    @Test
+    void mediaAritmeticaVacio() {
+        Estadistica.mediaAritmetica();
+    @Test
+    void varianza() {
+    }
+
+    @Test
+    void desviacionEstandar() {
+    }
+
+    @Test
+    void menu() {
+    }
 }
