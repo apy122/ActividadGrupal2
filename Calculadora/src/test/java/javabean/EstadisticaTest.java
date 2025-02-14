74e6dcc2c32c376515885d9430a8d634c9fe0417
david-sanara <david.sanchez769@comunidadunir.net>
2025-02-14 19:37:09 +0100

    EstadisticaTest realizado y todo correcto. Tambien se suben dos archivos de configuracion

--- Calculadora/src/test/java/javabean/EstadisticaTest.java
+++ Calculadora/src/test/java/javabean/EstadisticaTest.java
@@ -3,6 +3,9 @@ package javabean;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class EstadisticaTest {
 
     private Estadistica estadistica;
@@ -10,41 +13,48 @@ public class EstadisticaTest {
     @BeforeEach
     void setUp() {
 
-        Estadistica estadistica = new Estadistica()
+        Estadistica estadistica = new Estadistica();
     }
 
     @Test
     void mediaAritmeticaPositivos() {
-        double[] numeros = {1,7,9,5}
+        double[] numeros = new double[]{1, 7, 9, 5};
 
-        double resultado = Estadistica.mediaAritmetica(numeros);
-
-
-
+        assertEquals(5.5,Estadistica.mediaAritmetica(numeros));
 
     }
     @Test
     void mediaAritmeticaNegativos() {
-        Estadistica.mediaAritmetica(-1,-7,-9,-5);
+        double[] numeros = new double[]{-1, -7, -9, -5};
+        assertEquals(-5.5,Estadistica.mediaAritmetica(numeros));
 
     }
    @Test
-   void mediaAritmeticaCero(0) {
-       Estadistica.mediaAritmetica();
+   void mediaAritmeticaCero() {
+       double[] numeros = new double[]{0, 0, 0, 0};
+       assertEquals(0,Estadistica.mediaAritmetica(numeros));
 
     }
     @Test
     void mediaAritmeticaVacio() {
-        Estadistica.mediaAritmetica();
+    double[] numeros = new double[0];
+        assertThrows(IllegalArgumentException.class, () -> Estadistica.mediaAritmetica(numeros));
+    }
+
     @Test
     void varianza() {
+        double[] numeros = new double[]{1, 7, 9, 5};
+        assertEquals(8.75,Estadistica.varianza(numeros));
     }
 
     @Test
     void desviacionEstandar() {
+        double[] numeros = new double[]{1, 7, 9, 5};
+        assertEquals(2.95,Estadistica.desviacionEstandar(numeros), 0.01);
     }
 
     @Test
     void menu() {
+
     }
 }
