293f889fc283a295fb21d3484bf6f782c3bcf50a
david-sanara <david.sanchez769@comunidadunir.net>
2025-02-19 11:43:54 +0100

    Correción ArithmeticException y mejora en JavaDoc

--- Calculadora/src/main/java/javabean/Modulo.java
+++ Calculadora/src/main/java/javabean/Modulo.java
@@ -3,23 +3,35 @@ package javabean;
 import java.util.Scanner;
 
 /** Clase Módulo dentro de la Calculadora grupal.
- * <p>Ejemplo de uso</p>
+ *
+ * <p>Esta clase devuelve el módulo de dos números o el valor absoluto de un número.
+ * Para elegir qué opción queremos, añado un menú.</p>
+ *
+ * <h2>Ejemplo de uso</h2>
+ * <pre>{@code
+ *  int resultadoModulo = Modulo.modulo(10, 3); // Devuelve 1
+ *  double valorAbs = Modulo.vAbsoluto(-3.5);  // Devuelve 3.5
+ * }</pre>
+ *
  * @author David Sánchez
- * @version 5.0
+ * @version 6.0
+ *
  * V. 2 Corregida función "modulo", sustituyendo return por throw new en la excepción.
  * V. 3. Inserté static en las funciones "módulo" y "vAbsolut" para poder incluirla en el menú
  * V. 4. No logré usar la excepción. Modifico el mét_odo 1 y añado un return 0 y un mensaje.
- * V. 5. Tras trabajar con el compañero que me testea (Alessandro) vuelvo a usar ArithmeticException.
+ * V. 5. Tras trabajar con el compañero que me testea (Alessandro) vuelvo a usar
+ *  ArithmeticException.
+ * V. 6. He añadido try-catch para poder manejar ArithmeticException. Además, mejoro el JavaDoc.
  */
 
 public class Modulo {
 
     /**
-     * Esta clase tiene dos funciones:
-     * 1º Cálculo del módulo de dos números enteros. (Residuo de una división entera).
+     * Cálculo del módulo de dos números enteros. (Residuo de una división entera).
      * @param a es el dividendo.
      * @param b es el divisor.
      * @return módulo entre a y b.
+     * @throws ArithmeticException si el divisor es cero.
      */
 
     public static int modulo(int a, int b){
@@ -31,7 +43,7 @@ public class Modulo {
     }
 
     /**
-     * 2ª Función. Cálculo del valor absoluto de un número.
+     * Cálculo del valor absoluto de un número.
      * @param a el número del que queremos calcular el valor absoluto.
      * @return valor absoluto de a.
      * Como los valores absolutos son siempre positivos, añadimos un if.
@@ -46,12 +58,18 @@ public class Modulo {
     }
 
     /**
-     * Menú para elegir cual de los dos métodos módulo queremos usar
-     * La primera opción realiza la función módulo con dos números insertados.
-     * La segunda función nos da el valor absoluto del número insertado.
+     * Menú para elegir que función queremos usar.
+     * <ul>
+     *     <li>La primera opción realiza la función módulo con dos números
+     *     insertados. Añado un try, que prueba a ejecutar el módulo, y un catch,
+     *     que captura el throw ArithmeticException y devuelve mensaje de error.</li>
+     *
+     *     <li>La segunda opción nos da el valor absoluto del número insertado.</li>
+     *
+     *     <li>La tercera opción, vuelve al menú principal de la Calculadora.</li>
+     * </ul>
      * Uso @param Scanner para que se puedan insertar parámetros en la terminal.
      * Como las opciones serán numéricas será variable int (entero).
-     * En el caso 1 damos el resultado del módulo con resultMod.
      */
 
     public static void modMenu(){
@@ -72,8 +90,12 @@ public class Modulo {
                     System.out.println("Ingresa el segundo valor: ");
                     int b = leer.nextInt();
 
-                    int resultMod = modulo(a,b);
-                    System.out.println("El modulo es: " + resultMod);
+                    try {
+                        int resultMod = modulo(a, b);
+                        System.out.println("El modulo es: " + resultMod);
+                    } catch (ArithmeticException errorCero) {
+                        System.out.println("Error! " + errorCero.getMessage());
+                    }
                     break;
 
                  case 2:
