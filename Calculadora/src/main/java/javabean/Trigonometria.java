2c51ff8d2032e4a6591691cd7e8d8d7985691737
JavierCalvoHidalgo <javier.calvo962@comunidadunir.net>
2025-02-20 12:06:24 +0100

    ADD: se añade TrigonometriaTest

--- Calculadora/src/main/java/javabean/Trigonometria.java
+++ Calculadora/src/main/java/javabean/Trigonometria.java
@@ -1,4 +1,134 @@
 package javabean;
 
+
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * La clase trigonometría tendrá métodos para resolver seno coseno y tangente en la calculadora
+ *
+ * @author Mario Ortega Navarro (aka mortegaDAM@github.com)
+ * @version 0.1
+ */
+
 public class Trigonometria {
+
+    private List<String> menu;
+    private double seno, coseno, tangente, angulo;
+
+    //constructor vacío
+    public Trigonometria() {
+    }
+
+    //Getter and setter
+
+    public double getSeno() {
+        return seno;
+    }
+
+    public void setSeno(double seno) {
+        this.seno = seno;
+    }
+
+    public double getCoseno() {
+        return coseno;
+    }
+
+    public void setCoseno(double coseno) {
+        this.coseno = coseno;
+    }
+
+    public double getTangente() {
+        return tangente;
+    }
+
+    public void setTangente(double tangente) {
+        this.tangente = tangente;
+    }
+
+    public double getAngulo() {
+        return angulo;
+    }
+
+    public void setAngulo(double angulo) {
+        this.angulo = angulo;
+    }
+
+    /**
+     * Metodo para conseguir el coseno de un angulo
+     */
+
+    public double seno(double angulo){
+        return Math.sin(angulo);
+    }
+
+    /**
+     * metodo para el coseno de un angulo
+     */
+
+    public double coseno(double angulo){
+        return Math.cos(angulo);
+    }
+
+    /**
+     * metodo para conseguir la tangente de un angulo
+     */
+
+    public double tangente(double angulo){
+        return seno(angulo) / coseno(angulo);
+    }
+
+    /**
+     * Menú trigonometria con cuatro opciones diferentes de operación, además de una de salida del menú trigonometria:
+     *      1 Seno de un angulo(radianes).
+     *      2 Coseno de un angulo(radianes).
+     *      3 Tangente de un angulo(radianes).
+     *      4 Salida del menu Trigonometria
+     * Se incluye también el objeto trigonometria para poder realizar las operaciones.
+     * Se crea una variable int del Scanner para poder seleccionar una opción del menú.
+     */
+
+    public static void menuTrigonometria(){
+        Scanner leer = new Scanner(System.in);
+        Trigonometria trigonometria = new Trigonometria();
+        int opcionTrigonometria;
+
+        do {
+            System.out.println("Seleciona una opción para realizar una operacion de trigonometria:");
+            System.out.println("1. Seno de un angulo (radianes) ");
+            System.out.println("2. Coseno de un angulo (radianes)");
+            System.out.println("3. Tangente de un angulo (radianes)");
+            System.out.println("4. Salida del menú Trigonometria");
+
+            opcionTrigonometria = leer.nextInt();
+            double angulo;
+
+            switch (opcionTrigonometria) {
+                case 1:
+                    System.out.println("Introduzca el angulo en radianes");
+                    angulo = leer.nextDouble();
+                    System.out.println("El seno del angulo es : " + trigonometria.seno(angulo));
+                    break;
+
+                case 2:
+                    System.out.println("Introduzca el angulo en radianes");
+                    angulo = leer.nextDouble();
+                    System.out.println("El coseno del angulo es : " + trigonometria.coseno(angulo));
+                    break;
+
+                case 3:
+                    System.out.println("Introduzca el angulo en radianes");
+                    angulo = leer.nextDouble();
+                    System.out.println("La tangente del angulo es : " + trigonometria.tangente(angulo));
+                    break;
+
+
+            }
+
+        }
+        while (opcionTrigonometria != 4);
+
+
+    }
 }
+
--- Calculadora/src/test/java/javabean/TrigonometriaTest.java
+++ Calculadora/src/test/java/javabean/TrigonometriaTest.java
@@ -1,4 +1,35 @@
 package javabean;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TrigonometriaTest {
+
+    private Trigonometria trigonometria;
+    @BeforeEach
+    void setUp() {
+        trigonometria = new Trigonometria();
+    }
+
+    @Test
+    void trigonometriaSeno() {
+        assertEquals(1, trigonometria.seno(90), "El resultado de seno de 90 debe ser 1.");
+        assertEquals(-0.70710678, trigonometria.seno(-45), "El resultado de seno de -45 debe ser -0.70710678.");
+    }
+
+    @Test
+    void trigonometriaCoseno() {
+        assertEquals(0, trigonometria.coseno(90), "El resultado de coseno de 90 debe ser 0.");
+        assertEquals(0.70710678, trigonometria.coseno(-45), "El resultado de coseno de -45 debe ser 0.70710678.");
+    }
+
+    @Test
+    void trigonometriaTangente() {
+        assertEquals(-5.67128 , trigonometria.tangente(100), "El resultado de tangente de 100 debe ser −5.67128.");
+        assertEquals(-1, trigonometria.tangente(-45), "El resultado de tangente de -45 debe ser -1.");
+    }
+
+
 }
