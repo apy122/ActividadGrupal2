5e309d87150065607b23f63ea02a16297b1397f9
Mario Ortega <mario.ortega303@comunidadunir.net>
2025-02-21 18:44:26 +0100

    añadidas pruebas unitarias de cociente

--- Calculadora/src/test/java/javabean/CocienteTest.java
+++ Calculadora/src/test/java/javabean/CocienteTest.java
@@ -4,8 +4,38 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CocienteTest {
 
+    Cociente cociente;
 
+    @BeforeEach
+    void setUp() {
+        cociente = new Cociente();
+    }
+
+    @Test
+    void cocienteEntero() {
+        assertEquals(8 ,cociente.cocienteEntero(32,4));
+        assertThrows(IllegalArgumentException.class, () -> cociente.cocienteEntero(4,0));
+    }
+
+    @Test
+    void cocienteReal() {
+        assertEquals( 5.888888888888889 ,cociente.cocienteReal(26.5,4.5));
+        assertThrows(IllegalArgumentException.class, () -> cociente.cocienteReal(4.6,0));
+    }
+
+    @Test
+    void inverso() {
+        assertEquals(5, cociente.inverso(-5));
+        assertEquals(-25.5, cociente.inverso(25.5));
+    }
+
+    @Test
+    void raiz() {
+        assertEquals(6, cociente.raiz(36));
+        assertThrows(IllegalArgumentException.class, () -> cociente.raiz(-25));
+    }
 }
